72d862c2f6829ed25ba819c56c96a1e25453751d
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-19 19:33:18 -0300

    finished

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using Project.Entities;
 
 namespace Project
 {
@@ -7,7 +9,38 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            
+            System.Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine($"{prod.Name},{prod.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                System.Console.WriteLine("Exception error: " + e.Message);
+            }
         }
     }
 }
